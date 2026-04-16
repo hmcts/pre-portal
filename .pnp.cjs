651ab2ebef788b84f733537eb46f495024ef00ba
ruth.bovell <ruth.bovell@justice.gov.uk>
2026-04-16 12:04:00 +0100

    upgrade marked

--- .pnp.cjs
+++ .pnp.cjs
@@ -83,7 +83,7 @@ const RAW_RUNTIME_STATE =
           ["jose", "npm:6.2.2"],\
           ["lint-staged", "npm:16.4.0"],\
           ["lodash", "npm:4.18.1"],\
-          ["marked", "npm:15.0.12"],\
+          ["marked", "npm:18.0.0"],\
           ["mini-css-extract-plugin", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:2.10.2"],\
           ["multer", "npm:2.1.1"],\
           ["node-mocks-http", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:1.17.2"],\
@@ -15512,10 +15512,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["marked", [\
-      ["npm:15.0.12", {\
-        "packageLocation": "./.yarn/cache/marked-npm-15.0.12-878d4bfa62-deeb619405.zip/node_modules/marked/",\
+      ["npm:18.0.0", {\
+        "packageLocation": "./.yarn/cache/marked-npm-18.0.0-6df19feaa2-eb746a1f6e.zip/node_modules/marked/",\
         "packageDependencies": [\
-          ["marked", "npm:15.0.12"]\
+          ["marked", "npm:18.0.0"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -17545,7 +17545,7 @@ const RAW_RUNTIME_STATE =
           ["jose", "npm:6.2.2"],\
           ["lint-staged", "npm:16.4.0"],\
           ["lodash", "npm:4.18.1"],\
-          ["marked", "npm:15.0.12"],\
+          ["marked", "npm:18.0.0"],\
           ["mini-css-extract-plugin", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:2.10.2"],\
           ["multer", "npm:2.1.1"],\
           ["node-mocks-http", "virtual:16701266ea8f9c4c47e5d4e7d6e5f60f5b90bd85760be31472205688a0f541b309f8b385bcdfaeae586863ad36065412b38f52d324d6841d21abacc4b09d6704#npm:1.17.2"],\
