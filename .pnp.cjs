c2d23f8820316c28b2d9769e0f92f4ff997c14b7
Lydia Ralph <lydia.ralph@justice.gov.uk>
2026-07-24 09:46:15 +0100

    Remove resolution for brace-expansion to resolve security alert

--- .pnp.cjs
+++ .pnp.cjs
@@ -9025,11 +9025,11 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:5.0.6", {\
-        "packageLocation": "./.yarn/cache/brace-expansion-npm-5.0.6-abf39a1281-a7acf120fe.zip/node_modules/brace-expansion/",\
+      ["npm:5.0.8", {\
+        "packageLocation": "./.yarn/cache/brace-expansion-npm-5.0.8-08bf144160-75d2d2ebc8.zip/node_modules/brace-expansion/",\
         "packageDependencies": [\
           ["balanced-match", "npm:4.0.4"],\
-          ["brace-expansion", "npm:5.0.6"]\
+          ["brace-expansion", "npm:5.0.8"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -15727,7 +15727,7 @@ const RAW_RUNTIME_STATE =
       ["npm:10.2.4", {\
         "packageLocation": "./.yarn/cache/minimatch-npm-10.2.4-11f0605299-aea4874e52.zip/node_modules/minimatch/",\
         "packageDependencies": [\
-          ["brace-expansion", "npm:5.0.6"],\
+          ["brace-expansion", "npm:5.0.8"],\
           ["minimatch", "npm:10.2.4"]\
         ],\
         "linkType": "HARD"\
@@ -15735,7 +15735,7 @@ const RAW_RUNTIME_STATE =
       ["npm:10.2.5", {\
         "packageLocation": "./.yarn/cache/minimatch-npm-10.2.5-f1c8297822-19e87a931a.zip/node_modules/minimatch/",\
         "packageDependencies": [\
-          ["brace-expansion", "npm:5.0.6"],\
+          ["brace-expansion", "npm:5.0.8"],\
           ["minimatch", "npm:10.2.5"]\
         ],\
         "linkType": "HARD"\
@@ -15759,7 +15759,7 @@ const RAW_RUNTIME_STATE =
       ["npm:9.0.7", {\
         "packageLocation": "./.yarn/cache/minimatch-npm-9.0.7-145dd43fbb-72b9ba4af3.zip/node_modules/minimatch/",\
         "packageDependencies": [\
-          ["brace-expansion", "npm:5.0.6"],\
+          ["brace-expansion", "npm:5.0.8"],\
           ["minimatch", "npm:9.0.7"]\
         ],\
         "linkType": "HARD"\
